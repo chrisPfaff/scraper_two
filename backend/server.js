eed5d4cff3b29bf26deb473f4e1ee0787e39597c
Chris Pfaff <chrispfaff10@gmail.com>
2022-05-19 14:44:22 -0500

    added cors package

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 const fastify = require("fastify")({ logger: true });
 const puppeteer = require("puppeteer");
 
+fastify.register(require("@fastify/cors"), {
+  origin: "*",
+});
+
 fastify.get("/hello", async (request, reply) => {
   let webpage = request.query.webpage;
   let element = request.query.element;
-  console.log(webpage, element);
   (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(webpage);
     const elements = await page.$(element);
+    // const results = [];
+    // elements.forEach(async (element) => {
+    //   await results.push(element.getProperty("innerHTML"));
+    // });
     const val = await page.evaluate((el) => el.innerText, elements);
     reply.send(val).code(200);
     await browser.close();
