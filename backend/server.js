17c877d48a99a4495021908a6f40c443881e1dd1
Chris Pfaff <chrispfaff10@gmail.com>
2022-05-18 20:33:37 -0500

    added functionality for element param

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ const fastify = require("fastify")({ logger: true });
 const puppeteer = require("puppeteer");
 
 fastify.get("/hello", async (request, reply) => {
-  let params = request.query.webpage;
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(params);
-    await page.screenshot({ path: "example.png" });
+  let webpage = request.query.webpage;
+  let element = request.query.element;
+  console.log(webpage, element);
+  // (async () => {
+  //   const browser = await puppeteer.launch();
+  //   const page = await browser.newPage();
+  //   await page.goto(params);
+  //   await page.screenshot({ path: "example.png" });
 
-    await browser.close();
-  })();
+  //   await browser.close();
+  // })();
   reply.code(200);
 });
 
