e99628d9f1fda4edf74bb94ec888a48470fa660a
Chris Pfaff <chrispfaff10@gmail.com>
2022-05-19 16:44:03 -0500

    puppeteer grabbing all elements instead of one

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,18 @@ fastify.get("/hello", async (request, reply) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(webpage);
-    const elements = await page.$(element);
-    // const results = [];
-    // elements.forEach(async (element) => {
-    //   await results.push(element.getProperty("innerHTML"));
-    // });
-    const val = await page.evaluate((el) => el.innerText, elements);
-    console.log(val);
-    reply.send({ data: val }).code(200);
+    const elements = await page.$$(element);
+    for (let i = 0; i < elements.length; i++) {
+      const htmlElement = elements[i];
+      const textElement = await page.evaluate(
+        (htmlElement) => htmlElement.textContent,
+        htmlElement
+      );
+      console.log(textElement);
+    }
+
+    //const val = await page.evaluate((el) => el.innerText, elements);
+    //reply.send({ data: val }).code(200);
     await browser.close();
   })();
 });
