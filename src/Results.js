d1d75bb6ed48a790545fb40ba05c2940abd00963
Chris Pfaff <chrispfaff10@gmail.com>
2022-05-20 09:45:51 -0500

    rendering correctly

--- src/Results.js
+++ src/Results.js
@@ -2,19 +2,15 @@ export default function Results(data) {
   data = data.results;
   const render = () => {
     if (data !== undefined) {
-      return data.map((result) => {
-        return (
-          <div className="result">
-            <p>{result}</p>
-          </div>
-        );
-      });
+      return (
+        <div className="results">
+          <h2>Results</h2>
+          {data.map((result) => {
+            return <p>{result}</p>;
+          })}
+        </div>
+      );
     }
   };
-  return (
-    <div className="results">
-      <h2>Results</h2>
-      {render()}
-    </div>
-  );
+  return render();
 }
