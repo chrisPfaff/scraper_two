97dcd4231ffeb9df707eeff79963a1215d965d4a
Chris Pfaff <chrispfaff10@gmail.com>
2022-06-23 18:33:06 -0500

    added state to see if user is already created if not signup page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Search from "./Search";
 import Results from "./Results";
 import SignUp from "./SignUp";
+import Login from "./Login";
 import Modal from "./Modal";
 import { useState, useEffect } from "react";
 import "./styles/app.scss";
@@ -8,6 +9,7 @@ import "./styles/app.scss";
 export default function MyApp() {
   const [loading, setLoading] = useState(true);
   const [showModal, setShowModal] = useState(true);
+  const [userNotCreated, setUserNotCreated] = useState(false);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -40,7 +42,16 @@ export default function MyApp() {
     <main>
       {loading ? (
         <Modal>
-          <SignUp closeModal={toggleModal} />
+          <Login
+            closeModal={toggleModal}
+            userCreate={setUserNotCreated}
+            user={userNotCreated}
+          />
+        </Modal>
+      ) : null}
+      {userNotCreated ? (
+        <Modal>
+          <SignUp closeModal={toggleModal} userCreate={setUserNotCreated} />
         </Modal>
       ) : null}
       {loading ? null : (
